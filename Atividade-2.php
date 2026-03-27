f29b30a027314301006980b26422de10073d069c
Felipe Placo <felipe.placo@unesp.br>
2026-03-26 22:51:20 -0300

    Atualizado

--- Atividade-2.php
+++ Atividade-2.php
@@ -1,4 +1,3 @@
-<<<<<<<< HEAD:Atividade-2.php
 <?php
 
 $cidade = "Bauru";
@@ -18,25 +17,4 @@ echo "<html>" .
 # utilizei as variáveis concatenadas para formar a mensagem:
 # Bauru, 01 de Marco de 2024.
 
-========
-<?php
-
-$cidade = "Bauru";
-$dia = "01";
-$ano = "2024";
-$mes = "Marco";
-
-echo "<html>" .
-     "<body>" .
-     "<p><b>" . $cidade . "</b>, " . $dia . " de " . $mes . " de " . $ano . "</p>" .
-     "</body>" .
-     "</html>";
-
-# Além da estrutura básica, comecei declarando as variáveis cidade, dia, ano e mês,
-# definindo o valor de cada uma delas, sendo: Bauru, 01, 2024 e Marco, respectivamente.
-# Depois, na hora de adicionar o parágrafo dentro do body, em vez de escrever cada nome,
-# utilizei as variáveis concatenadas para formar a mensagem:
-# Bauru, 01 de Marco de 2024.
-
->>>>>>>> 17ebebc50c5e7b72ad1c0eb78106258e9ec2c614:Atividade1.php
 ?>
