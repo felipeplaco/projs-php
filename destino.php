598ddba8e22d45784758072cf3dbab112f4d5eb4
Seu Nome <ra2557056@LDI311>
2026-04-23 20:15:46 -0300

    Aula 8

---
+++ destino.php
@@ -0,0 +1,12 @@
+<table border="1">
+    <tr>
+        <th>Nome</th>
+        <th>Celular</th>
+        <th>Cidade</th>
+    </tr>
+    <tr>
+        <td><?php echo $_POST['nome']; ?></td>
+        <td><?php echo $_POST['celular']; ?></td>
+        <td><?php echo $_POST['cidade']; ?></td>
+    </tr>
+</table>
---
+++ forms.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>Formulário</title>
+</head>
+<body>
+
+<h2>Cadastro</h2>
+
+<form action="destino.php" method="post">
+    Nome: <input type="text" name="nome"><br><br>
+    Celular: <input type="text" name="celular"><br><br>
+    Cidade: <input type="text" name="cidade"><br><br>
+
+    <input type="submit" value="Enviar">
+</form>
+
+</body>
+</html>
